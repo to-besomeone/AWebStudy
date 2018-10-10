56cc37f462c69b99614e00056ef992ba1effead2
gimjiu <evillion99@gmail.com>
2018-10-11 01:14:44 +0900

    following books about routing(URI)

--- app.py
+++ app.py
@@ -7,6 +7,13 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+@app.route('/profile/<username>')
+def get_profile(username):
+    return 'profile : ' + username
+
+@app.route('/message/<int:message_id>')
+def get_message(message_id):
+    return 'message_id : %d' % message_id
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port='8000', debug=True)
