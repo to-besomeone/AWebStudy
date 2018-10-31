2cfbc6451a1538d819828744b609e74408310725
gimjiu <evillion99@gmail.com>
2018-10-31 20:36:56 +0900

    181031 - jinja 전

--- app.py
+++ app.py
@@ -7,12 +7,14 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
+
 @app.route('/profile/<username>')
 def get_profile(username):
     return 'profile : ' + username
 
 
 if __name__ == '__main__':
+    # app.run(host='0.0.0.0', port=8000, debug='True')
     with app.test_request_context():
         print(url_for('hello_world'))
         print(url_for('get_profile', username='flask'))
