736f97f7b5494726b5f80806568f0c7ab9d6afe3
gimjiu <evillion99@gmail.com>
2018-10-31 22:06:23 +0900

    Jinja2 - html 연결 드디어 끝

--- app.py
+++ app.py
@@ -16,6 +16,6 @@ def get_profile(username):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000, debug='True')
-    with app.test_request_context():
-        print(url_for('hello_world'))
-        print(url_for('get_profile', username='flask'))
+    # with app.test_request_context():
+        # print(url_for('hello_world'))
+        # print(url_for('get_profile', username='flask'))
