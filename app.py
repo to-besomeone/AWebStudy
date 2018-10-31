b8040e2410db60429cd2370300bf51d5fa827ec9
gimjiu <evillion99@gmail.com>
2018-10-31 21:32:59 +0900

    Jinja2 - html 연결

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/hello/')
-def hello_world():
-    return 'Hello World!'
+@app.route('/hello/<name>')
+def hello_world(name=None):
+    return render_template('hello.html', name=name)
 
 
 @app.route('/profile/<username>')
@@ -14,7 +15,7 @@ def get_profile(username):
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8000, debug='True')
-    with app.test_request_context():
-        print(url_for('hello_world'))
-        print(url_for('get_profile', username='flask'))
+    app.run(host='0.0.0.0', port=8000, debug='True')
+    # with app.test_request_context():
+    #     print(url_for('hello_world'))
+    #     print(url_for('get_profile', username='flask'))
